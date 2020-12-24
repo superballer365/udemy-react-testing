9b13d36754206ce7685c43154afecf2034369318
Matthew Balnis <matthew.balnis@uconn.edu>
2020-12-23 20:07:28 -0500

    Added GuessedWords component and tests

---
+++ jotto-app/src/GuessedWords.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GuessedWords = props => {
+  return (
+    <div data-test="component-guessed-words">
+      {props.guessedWords.length === 0 ? (
+        <span data-test="guess-instructions">Guess a word!</span>
+      ) : (
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+              <tbody>
+                {props.guessedWords.map((word, index) => (
+                  <tr key={index} data-test="guessed-word">
+                    <td>{word.guessedWord}</td>
+                    <td>{word.letterMatchCount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </thead>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
+
+export default GuessedWords;
